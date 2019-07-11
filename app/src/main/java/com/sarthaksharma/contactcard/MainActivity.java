1eaa675ff4a70a9d48d546c14e5202e07fd6834d
InvincibleSarthak <eccentric.sarthak@gmail.com>
2019-07-11 22:27:08 +0530

    Activity 1 and 2 demo ready, Activity 3 starting step

--- app/src/main/java/com/sarthaksharma/contactcard/MainActivity.java
+++ app/src/main/java/com/sarthaksharma/contactcard/MainActivity.java
@@ -1,7 +1,10 @@
 package com.sarthaksharma.contactcard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this,Main2Activity.class);
+        startActivity(intent);
+        Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show();
     }
 }
