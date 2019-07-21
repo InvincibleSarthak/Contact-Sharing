2df50f847270088eb27cddac7aa6b036acaee1e5
InvincibleSarthak <eccentric.sarthak@gmail.com>
2019-07-21 19:32:17 +0530

    LOGIN page changed, OTP Fragment added

--- app/src/main/java/com/sarthaksharma/contactcard/MainActivity.java
+++ app/src/main/java/com/sarthaksharma/contactcard/MainActivity.java
@@ -1,28 +1,29 @@
 package com.sarthaksharma.contactcard;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn=findViewById(R.id.login);
     }
 
     public void login(View view) {
-        Intent intent = new Intent(this,Main4Activity.class);
-        startActivity(intent);
+        btn.setVisibility(View.GONE);
         Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show();
-    }
-
-    public void signUp(View view) {
-        Intent intent = new Intent(this, Main3Activity.class);
-        startActivity(intent);
-        Toast.makeText(this,"Button Clicked",Toast.LENGTH_SHORT).show();
+        FragmentManager fm = getSupportFragmentManager();
+        OTPScreen otp = new OTPScreen();
+        fm.beginTransaction().replace(R.id.Main, otp).commit();
     }
 }
